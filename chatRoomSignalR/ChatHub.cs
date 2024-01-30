ef1e3825199e637c8bc945f7a0d2933a8f2f3d45
Andelson Lopez <69158247+l9dson-wq@users.noreply.github.com>
2024-01-30 16:47:42 -0500

    SignalR chat room
    SignalR chat room

---
+++ chatRoomSignalR/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace chatRoomSignalR
+{
+  public class ChatHub : Hub
+  {
+    public async Task SendMessage(string room, string user, string message)
+    {
+      await Clients.Group(room).SendAsync("ReceiveMessage", user, message);
+    }
+
+    public async Task AddToGroup(string room)
+    {
+      await Groups.AddToGroupAsync(Context.ConnectionId, room);
+      await Clients.Group(room).SendAsync("ShowWho", $"Somebody just entered the chat {Context.ConnectionId}");
+    }
+  }
+}
---
+++ chatRoomSignalR/Controllers/ChatController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace chatRoomSignalR.Controllers
+{
+  public class ChatController : Controller
+  {
+
+    public static Dictionary<int, string> Rooms =
+      new()
+      {
+        { 1, "Cervezas" },
+        { 2, "Programacion" },
+        { 3, "Moda" }
+      };
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Room(int room)
+    {
+      return View("Room", room);
+    }
+  }
+}
